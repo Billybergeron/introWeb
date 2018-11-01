cf51c15a8bd8a59fa5c3b5892bbfd777b01830b4
Billybergeron <berbi1733260@etu.cegepjonquiere.ca>
2018-11-01 12:07:40 -0400

    5ieme exercice 5ieme exercice V2

--- exercices/Exercices -  Si composé/JS/traitement12.js
+++ exercices/Exercices -  Si composé/JS/traitement12.js
@@ -1,5 +1,23 @@
 function btnCalculer_onclick()
-{ 
- 	
+{
+    var Nbr1,Nbr2,Nbr3,Nbr4;
 
+    Nbr4 = 3;
+    Nbr1 = parseFloat(document.getElementById("txtNbre1").value);
+    Nbr2 = parseFloat(document.getElementById("txtNbre2").value);
+    Nbr3 = parseFloat(document.getElementById("txtNbre3").value);
+
+    if (Nbr1 >= Nbr2 && Nbr2 != Nbr3)
+    {
+        Nbr4 = Nbr4 + 2 * Nbr1
+    }
+    else if (Nbr1 == Nbr2 || Nbr2 > Nbr3)
+    {
+        Nbr4 = Nbr4 + Nbr1
+    }
+    else
+    {
+        Nbr4 = Nbr4 * Nbr2 / Nbr1 - Nbr3
+    }
+    document.getElementById("lblMessage").innerHTML = "le signe du résultat est " + Nbr4;
 }
